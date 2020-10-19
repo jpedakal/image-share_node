33f9ecc8cb3dcf84cfae0aab1cb33ccf0a241c10
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-10-19 08:37:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const Port = 5000;
 app.listen(Port, () => {
     console.log(`Server Running on Port ${Port}`)
