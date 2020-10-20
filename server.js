b73efcb5cf3554533724a292b242da96bb6d1fde
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-10-20 17:15:34 +0530

    update

--- server.js
+++ server.js
@@ -5,11 +5,19 @@ require('dotenv').config();
 const mongo = require('./src/database/mongo_db');
 const addProduct = require('./src/api/add_product');
 const fetchProduct = require('./src/api/fetch_product');
+const fetchProductById = require('./src/api/fetch_productById');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    next();
+});
+
 app.use('/api', addProduct);
 app.use('/api', fetchProduct);
+app.use('/api', fetchProductById);
 
 const Port = 5000;
 app.listen(Port, () => {
